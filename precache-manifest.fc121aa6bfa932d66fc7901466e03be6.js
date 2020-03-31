4dae12374cc1cb3d0d0be469abde30067700c677
bashvtini <bashvtini@gmail.com>
2020-03-31 13:27:52 +0200

    Updates

---
+++ precache-manifest.fc121aa6bfa932d66fc7901466e03be6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a9dd41147a71e561a0168c650133180",
+    "url": "/learning-vault-ui/index.html"
+  },
+  {
+    "revision": "10d8f5d120e6ba75913d",
+    "url": "/learning-vault-ui/static/css/main.979c1af6.chunk.css"
+  },
+  {
+    "revision": "4a5e30055c29e6749dae",
+    "url": "/learning-vault-ui/static/js/2.6a66af25.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/learning-vault-ui/static/js/2.6a66af25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d8f5d120e6ba75913d",
+    "url": "/learning-vault-ui/static/js/main.58a8741a.chunk.js"
+  },
+  {
+    "revision": "290cae8ee4a5a4d5c11c",
+    "url": "/learning-vault-ui/static/js/runtime-main.7695d838.js"
+  }
+]);
